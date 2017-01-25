7746cad73ba3fc5e97af99845d5d5ed8495be935
U-DESKTOP-UJMLSS3\Grant <Grant@DESKTOP-UJMLSS3.mit.edu>
2017-01-25 18:07:41 -0500

    added level design tools YEEEEEEEEE

--- Assets/GrantTest/Scripts/GameController.cs
+++ Assets/GrantTest/Scripts/GameController.cs
@@ -1,39 +1,177 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
 
 public class GameController : MonoBehaviour {
     public GameObject gemPrefab;
     public GameObject map;
-    public float scroll_speed = .00001f;
-    public float spawnTime = 3f;
-    Queue<GameObject> gems = new Queue<GameObject>();
-    float last_spawn_time = 0;
+    public GameObject gemSlidePrefab;
+    public AudioSource audioSource;
 
+    //Spawn parameters
+    public float scroll_speed;
+    public float spawnTime;
+    float last_spawn_time = 0;
     float angle = 0;
+    float slideDiff = .05f;
+    float leveloffset = 3;
 
     // Use this for initialization
     void Start () {
+        LoadLevel("yee");
+        audioSource.Play(0);
+        Debug.Log(audioSource.GetType());
     }
-	
+
+    private bool LoadLevel(string levelName)
+    {
+        string fileName = "Assets/GrantTest/Audio/" + levelName + ".txt";
+        try
+        {
+            string line;
+            StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+
+            using (theReader)
+            {
+                line = theReader.ReadLine();
+                if (line != null)
+                {
+                    // While there's lines left in the text file, do this:
+                    float slideTime = 0;
+                    float slideStartAngle = 0;
+                    do
+                    {
+                        // Do whatever you need to do with the text line, it's a string now
+                        // In this example, I split it into arguments based on comma
+                        // deliniators, then send that array to DoStuff()
+                        string[] gems = line.Split('\t');
+                        float time = float.Parse(gems[0]);
+                        string[] entries = gems[1].Split(' ');
+
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        // FOR TESTING ONLY!!!!!
+                        if (entries[0] == "click")
+                        {
+                            entries[1] = "0";
+                        }
+                        else
+                        {
+                            entries[2] = "0";
+                        }
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+                        ///////////////////////////////////////////////////////////////////////
+
+
+                        if (entries[0] == "click")
+                        {
+                            SpawnGem(time, float.Parse(entries[1]));
+                        }
+                        if (entries[0] == "slide")
+                        {
+                            if (entries[1] == "start")
+                            {
+                                slideTime = time;
+                                slideStartAngle = float.Parse(entries[2]);
+
+                                SpawnGem(time, float.Parse(entries[2]));
+                            }
+                            else
+                            {
+                                float dt = (time - slideTime);
+                                int num_to_spawn = (int)Math.Floor(dt / slideDiff);
+                                dt = dt / num_to_spawn;
+                                float dAngle = (float.Parse(entries[2]) - slideStartAngle)/num_to_spawn;
+                                for (int i = 0; i < num_to_spawn; i++)
+                                {
+                                    SpawnSlideGem(slideTime + dt * (i + 1), slideStartAngle + dAngle * (i + 1));
+                                }
+
+                                slideTime = time;
+                                slideStartAngle = float.Parse(entries[2]);
+                            }
+                        }
+                        //if (entries[0] == "Click")
+                        //{
+                        //    Debug.Log(entries[1]);
+                        //}
+                        //if (entries[0] == "Slide")
+                        //{
+                        //    Debug.Log(entries[1]);
+                        //}
+
+                        line = theReader.ReadLine();
+                    }
+                    while (line != null);
+                }
+                // Done reading, close the reader and return true to broadcast success    
+                theReader.Close();
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Not Reading File");
+            return false;
+        }
+    }
+    
+
     void SpawnGem(float height, float angle)
     {
         GameObject gem = Instantiate(
                 gemPrefab,
-                new Vector3(2*Mathf.Sin(angle), height, 2*Mathf.Cos(angle)),
-                Quaternion.Euler(new Vector3(0, angle*180/Mathf.PI, 90))
+                new Vector3(2 * Mathf.Sin(angle * Mathf.PI / 180), (height + leveloffset )* scroll_speed , 2 * Mathf.Cos(angle * Mathf.PI / 180)),
+                Quaternion.Euler(new Vector3(0, angle, 90))
             ) as GameObject;
+        gem.SendMessage("setOffset", gameObject.transform.position.y);
         gem.SendMessage("setScrollSpeed", scroll_speed);
+        gem.SendMessage("setTime", height);
+        gem.SendMessage("setAudioSource", audioSource);
+    }
+    void SpawnSlideGem(float height, float angle)
+    {
+        GameObject gem = Instantiate(
+                gemSlidePrefab,
+                new Vector3(2 * Mathf.Sin(angle * Mathf.PI / 180), (height + leveloffset) * scroll_speed, 2 * Mathf.Cos(angle * Mathf.PI / 180)),
+                Quaternion.Euler(new Vector3(0, angle , 90))
+            ) as GameObject;
+        gem.SendMessage("setOffset",gameObject.transform.position.y);
+        Debug.Log("offset : " + gameObject.transform.position.y.ToString());
+        gem.SendMessage("setScrollSpeed", scroll_speed);
+        gem.SendMessage("setTime", height);
+        gem.SendMessage("setAudioSource", audioSource);
     }
 
-	// Update is called once per frame
-	void Update () {
-        if (Time.fixedTime - last_spawn_time > 1)
-        {
-            SpawnGem(10, angle);
-            last_spawn_time = Time.fixedTime;
-            angle += 10*Mathf.PI/180;
-        }
+
+    // Update is called once per frame
+    void Update () {
+        //if (Time.fixedTime - last_spawn_time > 1)
+        //{
+        //    SpawnGem(10, angle);
+        //    last_spawn_time = Time.fixedTime;
+        //    angle += 10*Mathf.PI/180;
+        //}
     }
 
     void FixedUpdate()
--- Assets/GrantTest/Scripts/GemController.cs
+++ Assets/GrantTest/Scripts/GemController.cs
@@ -4,11 +4,29 @@ using System.Collections;
 public class GemController : MonoBehaviour {
 
     float scroll_speed = 0;
+    float gemTime = 0;
+    float gem_offset = 0;
+    AudioSource audioBase = null;
 
+    public void setOffset(float offset)
+    {
+        gem_offset = offset;
+    }
     public void setScrollSpeed(float speed)
     {
         scroll_speed = speed;
     }
+
+    public void setTime(float time)
+    {
+        gemTime = time;
+    }
+
+    public void setAudioSource(AudioSource source)
+    {
+        audioBase = source;
+    }
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,9 +35,10 @@ public class GemController : MonoBehaviour {
     // is called once per frame
     void Update()
     {
+        Debug.Log(scroll_speed.ToString());
         gameObject.transform.position = new Vector3(
             gameObject.transform.position.x,
-            gameObject.transform.position.y - scroll_speed * Time.deltaTime,
+            scroll_speed * (gemTime - audioBase.time)+gem_offset,
             gameObject.transform.position.z);
 
         if (gameObject.transform.position.y <= 0)
