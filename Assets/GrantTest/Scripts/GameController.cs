6516996b60a368485fe9d52ead637cbbc7e219a3
U-DESKTOP-UJMLSS3\Grant <Grant@DESKTOP-UJMLSS3.mit.edu>
2017-01-25 11:55:34 -0500

    added gems falling in tunnel

---
+++ Assets/GrantTest/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+    public GameObject gemPrefab;
+    public GameObject map;
+    public float scroll_speed = .00001f;
+    public float spawnTime = 3f;
+    Queue<GameObject> gems = new Queue<GameObject>();
+    float last_spawn_time = 0;
+
+    float angle = 0;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+    void SpawnGem(float height, float angle)
+    {
+        GameObject gem = Instantiate(
+                gemPrefab,
+                new Vector3(2*Mathf.Sin(angle), height, 2*Mathf.Cos(angle)),
+                Quaternion.Euler(new Vector3(0, angle*180/Mathf.PI, 90))
+            ) as GameObject;
+        gem.SendMessage("setScrollSpeed", scroll_speed);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Time.fixedTime - last_spawn_time > 1)
+        {
+            SpawnGem(10, angle);
+            last_spawn_time = Time.fixedTime;
+            angle += 10*Mathf.PI/180;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/GrantTest/Scripts/GemController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemController : MonoBehaviour {
+
+    float scroll_speed = 0;
+
+    public void setScrollSpeed(float speed)
+    {
+        scroll_speed = speed;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(
+            gameObject.transform.position.x,
+            gameObject.transform.position.y - scroll_speed * Time.deltaTime,
+            gameObject.transform.position.z);
+
+        if (gameObject.transform.position.y <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
