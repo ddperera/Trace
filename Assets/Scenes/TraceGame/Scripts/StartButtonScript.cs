aa122f422ff81b67b0450d9aad4a27d91b4e4e8e
Sarah Tortorici <stortor@mit.edu>
2017-01-25 20:20:44 -0500

    Added a start menu that transitions into game when clicked

---
+++ Assets/Scenes/TraceGame/Scripts/StartButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Switch to the next scene
+	public void StartGame() {
+		SceneManager.LoadScene ("TraceGame");
+	}
+}
