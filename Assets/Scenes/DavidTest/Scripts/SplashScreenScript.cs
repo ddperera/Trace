6b02c37c7d3918f5af5d0b84f51d188a2847dabe
Sarah Tortorici <stortor@mit.edu>
2017-01-25 19:13:53 -0500

    Added splash screen and scene for start menu

---
+++ Assets/Scenes/DavidTest/Scripts/SplashScreenScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenScript : MonoBehaviour {
+
+	public float delayTime = 5;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (delayTime);
+
+		SceneManager.LoadScene ("StartMenu");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
