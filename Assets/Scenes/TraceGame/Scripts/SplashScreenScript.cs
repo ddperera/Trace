ba393354020747b46d5ea14335f20a0f4e8ee70f
Sarah Tortorici <stortor@mit.edu>
2017-01-27 12:11:47 -0500

    Added fading to the splash screen

--- Assets/Scenes/TraceGame/Scripts/SplashScreenScript.cs
+++ Assets/Scenes/TraceGame/Scripts/SplashScreenScript.cs
@@ -6,15 +6,47 @@ public class SplashScreenScript : MonoBehaviour {
 
 	public float delayTime = 5;
 
+	public float fadeSpeed = 0.3f;
+
+	public Texture fadeOutTexture;
+	private int drawDepth = -1000;
+	public float alpha = .99f;
+	public float fadeDir = -1.0f;
+	private Color alphaColor;
+
 	// Use this for initialization
 	IEnumerator Start () {
+		FadeIn();
 		yield return new WaitForSeconds (delayTime);
-
-		SceneManager.LoadScene ("StartMenu");
+		FadeOut();
 	}
 
-	// Update is called once per frame
-	void Update () {
+	// Use this for initialization
+	void OnGUI() 
+	{
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+		alphaColor = GUI.color;
+		alphaColor.a = alpha;
+		GUI.color = alphaColor;
+		GUI.depth = drawDepth;
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
 
+		if (alpha == 1)
+		{
+			//gameObject.tag = "Camera";
+			SceneManager.LoadScene ("StartMenu");
+		}
 	}
+
+	public void FadeIn()
+	{
+		fadeDir = -1;
+	}
+
+	public void FadeOut()
+	{
+		fadeDir = 1;
+	}
+		
 }
