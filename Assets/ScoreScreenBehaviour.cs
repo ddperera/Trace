a77c00ab70a03ddae3b3cad0900d2b8c09a3aa9a
Sarah Tortorici <stortor@mit.edu>
2017-01-29 15:40:17 -0500

    Added a score screen

---
+++ Assets/ScoreScreenBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScoreScreenBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Restart game (needs to be changed later to maintain song selection)
+	public void BackToGame()
+	{
+		SceneManager.LoadScene("TraceGame");
+	}
+
+	// Back to song selection screen
+	public void BackToSongSelection()
+	{
+		// This needs to be changed once song selection exists
+		SceneManager.LoadScene("StartMenu");
+	}
+}
