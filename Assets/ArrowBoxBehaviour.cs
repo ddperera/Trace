564381c2372e650ca95b7c433e81dd4711f056d1
David <David>
2017-01-30 16:14:35 -0500

    building arrow track visuals

---
+++ Assets/ArrowBoxBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowBoxBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject go = other.gameObject;
+        if (go.tag == "Gem")
+        {
+            GemBehaviour gemScript = go.GetComponent<GemBehaviour>();
+            gemScript.SetAsReady();
+        }
+    }
+}
